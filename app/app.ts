a1a659a03608f7730553aedf6e23006beda4dc72
Djibril Dieng <djibril.dieng@isen-lille.fr>
2021-04-06 20:57:19 +0200

    Demo course : TypeScript Getting Started

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,48 @@
 function startGame() {
     //starting a new game
 
-    let playerName: string = 'Audrey';
+    let playerName: string = getInputValue('playerName');
     logPlayer(playerName);
-    var messagesElement = document.getElementById('messages');
-    //messagesElement! assert that messagesElement is not null
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game ...';
+    
+    postScore(100, playerName);
+    postScore(-5, playerName);
 }
 
-function logPlayer(name: string) {
+function logPlayer(name: string = 'Player Name') : void {
     console.log(`New game starting for player : ${name}`);
 }
 
+function getInputValue(elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    if (inputElement.value === ''){
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore (score: number, playerName: string = 'Player Name') : void {
+
+    let logger: (value: string) => void;
+
+    if(score < 0){
+        logger = logError;
+    } 
+    else {
+        logger = logMessage;
+    }
+
+    const scoreElement: HTMLElement| null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score : ${score}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) : void => console.log(message);
+
+function logError(err: string) : void {
+    console.error(err);
+}
